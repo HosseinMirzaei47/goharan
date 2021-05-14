0db6a081156c217c9e28387bb878ff97bb490783
Hossein Mirzazadeh <hosseinhidd@gmail.com>
2021-05-14 16:15:50 +0430

    #10 - Grid RecyclerView
    **Modifications**
    - Changed bottom navigation's default item to "Services"
    - Implemented grid recyclerview

--- ui-services/src/main/java/com/carpet/goharshad/ServicesFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.carpet.goharshad
-
-import androidx.fragment.app.Fragment
-import com.carpet.goharshad.task.R
-
-class ServicesFragment : Fragment(R.layout.fragment_services)
---
+++ ui-services/src/main/java/com/carpet/goharshad/services/ServicesViewModel.kt
@@ -0,0 +1,8 @@
+package com.carpet.goharshad.services
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ServicesViewModel @Inject constructor() : ViewModel()
